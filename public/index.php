7bdfdeebe0b74581ce02ab2c55e9931bdaf570fc
Van Vreckem <svanvreckem@helb-prigogine.be>
2020-09-04 16:08:48 +0200

    Test

--- public/index.php
+++ public/index.php
@@ -8,7 +8,6 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
-
 $config = [
     "userClass" => User::class,
     "db"        => [
